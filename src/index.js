5ba3cd8e051f50869b82bc67712db5f179cfaae2
Applehole <skyheo123@naver.com>
2022-09-15 21:38:13 +0900

    Feat: Axios, redux add

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { Provider } from 'react-redux'
+import token from './store/token'
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+    <Provider store={token}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+)
