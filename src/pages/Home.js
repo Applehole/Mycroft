468107db1c62e46f8aebf8fe20b3b7c81ee27a11
Applehole <skyheo123@naver.com>
2022-09-16 17:20:07 +0900

    Feat: HomePage add

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { PageDefaultComponent } from '../styles/PageStyles/DefaultStyle'
+import { HomeComponenet } from '../styles/PageStyles/Home'
+import PictureUpload from '../components/PictureUpload'
 
 function Home() {
   return (
     <PageDefaultComponent>
-      <div>where am i?</div>
+      <HomeComponenet>
+        <PictureUpload />
+      </HomeComponenet>
     </PageDefaultComponent>
   )
 }
